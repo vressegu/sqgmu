e7dac0e73479a61ea28ebce10a90fa46d13aae9e
Valentin Resseguier <valentin.resseguier@inria.fr>
2016-03-18 10:52:15 +0100

    "Comment script init"

--- init.m
+++ init.m
@@ -1,7 +1,18 @@
+%%%%%%%%%%%%%%%%%%
+% Initialisation
+%%%%%%%%%%%%%%%%%%
+
+% Cleaning
 clear all;close all;clc;
+
+% Debug
 dbstop if error;
+
+% Paths
 fct = genpath([ pwd '/functions' ]);
 addpath(pwd)
 addpath(fct)
+
+% Cleaning
 clear fct
 home;
