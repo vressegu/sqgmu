1f4079d13b9c33e05b05c79439841410b5bbaea8
vressegu <valentin.resseguier@gmail.com>
2017-10-11 16:19:07 -0400

    Sink and source are both plotted with a sign +. The budgets (with correct signs) are computed in a second time. Moreover, the noise energy intake is computed with a conditional expectation (accurate but long  computation).

--- functions/advection/fct_dissip.m
+++ functions/advection/fct_dissip.m
@@ -15,53 +15,82 @@ ky=model.grid.k.ky;
 k2=model.grid.k.k2;
 kx1 = permute(kx,[ 3 4 1 2]);
 ky1 = permute(ky,[ 3 4 1 2]);
+k2=model.grid.k_HV.k2;
+
+%% Preprocessing
 
 % Boolean filter
 
 % Buoyancy
 b = real(ifft2(fft_b));
 
+% Gradient in Fourier space
+adv1x = 1i * kx .* fft_b;
+adv1y = 1i * ky .* fft_b;
+
+% Gradient in physical space
+gradb(:,:,1)=real(ifft2(adv1x));
+gradb(:,:,2)=real(ifft2(adv1y));
+
+% % Influence of the complex brownian variance
+% sigma_on_sq_dt = sqrt(prod(model.grid.MX)) * sigma_on_sq_dt;
+
 %% Advection term
 
-% warning('pk un signe - ici ?');
-% % Gradient in Fourier space
-% adv1x = - 1i * kx .* fft_b;
-% adv1y = - 1i * ky .* fft_b;
-% 
-% % Influence of the complex brownian variance
-% sigma_on_sq_dt = sqrt(prod(MX)) * sigma_on_sq_dt;
-% 
-% % Gradient in physical space
-% gradb(:,:,1)=real(ifft2(adv1x));
-% gradb(:,:,2)=real(ifft2(adv1y));
-% 
-% % % Advective term in physical space
-% % wgradT=sum(bsxfun(@times,w,gradb),3);
-% % % NB : in the stochastic case, w included both continuous and
-% % % non-continuous components of the velocity
-% % 
-% % % Advective term in Fourier space
-% % adv1=fft2(wgradT);clear wgradT
-% % 
-% % % Remove aliasing
-% % adv1(PX(1)+1,:)=0;
-% % adv1(:,PX(2)+1)=0;
-% % clear gradb
 
-noise_intake = zeros(size(b));
+
+if isinf(model.sigma.k_c) % Deterministic case
+    noise_intake = zeros(size(b));
+else % Stochastic case
+    % Fourier transform of white noise
+    dBt_C_on_sq_dt = fft2( randn( [ model.grid.MX 1 model.advection.N_ech]));
+    % Multiplication by the Fourier transform of the kernel \tilde \sigma
+    fft_sigma_dBt = bsxfun(@times,sigma_on_sq_dt,dBt_C_on_sq_dt);
+    clear dBt_C_on_sq_dt
+    % Homogeneous velocity field
+    sigma_dBt_dt = real(ifft2(fft_sigma_dBt));
+    clear fft_sigma_dBt
+    
+    
+    sigma_dBt_dtgradT2 = nan([model.grid.MX 1 model.advection.N_ech]);
+    
+    parfor sampl=1:model.advection.N_ech
+        % Advective term in physical space
+        sigma_dBt_dtgradT2(:,:,:,sampl)=sum(bsxfun(@times,sigma_dBt_dt(:,:,:,sampl),gradb),3);
+        % NB : in the stochastic case, w included both continuous and
+        % non-continuous components of the velocity
+        
+    end
+        % Remove aliasing
+%         sigma_dBt_dtgradT2(PX(1)+1,:,:,sampl)=0;
+%         sigma_dBt_dtgradT21(:,PX(2)+1,:,sampl)=0;
+    sigma_dBt_dtgradT2=fft2(sigma_dBt_dtgradT2);
+    sigma_dBt_dtgradT2(PX(1)+1,:,:,:)=0;
+    sigma_dBt_dtgradT2(:,PX(2)+1,:,:)=0;
+    sigma_dBt_dtgradT2=real(ifft2(sigma_dBt_dtgradT2));
+        
+        
+        %sigma_dBt_dtgradT2(:,:,:,sampl)=sigma_dBt_dtgradT2(:,:,:,sampl);
+    %end
+    
+    noise_intake = model.advection.dt_adv/2 * mean(sigma_dBt_dtgradT2.^2,4);
+    
+    
+end
+
 
 %% Laplacian diffusion term (from the stochastic material derivative)
-adv2 = - model.advection.coef_diff * k2 .* fft_b ;
-turb_dissip = - b .* real(ifft2(adv2));
+turb_dissip = model.advection.coef_diff * sum(gradb.^2,3);
+% adv2 = - model.advection.coef_diff * k2 .* fft_b ;
+% turb_dissip = - b .* real(ifft2(adv2));
 
 %% Hyperviscosity
 
-% Norm of wave vector
-k2=model.grid.k_HV.k2;
-
-% Hyperviscosity term
-adv4 = - model.advection.HV.val * k2 .^ (model.advection.HV.order/2) .* fft_b;
-dissip_HV = - b .* real(ifft2(adv4));
+% adv4 = - model.advection.HV.val * k2 .^ (model.advection.HV.order/2) .* fft_b;
+% dissip_HV = - b .* real(ifft2(adv4));
+adv4 = k2 .^ (model.advection.HV.order/4) .* fft_b;
+adv4 = real(ifft2(adv4));
+dissip_HV = model.advection.HV.val * adv4.^2;
 
 %% Forcing
 if isfield(model.advection, 'forcing') && model.advection.forcing.bool
